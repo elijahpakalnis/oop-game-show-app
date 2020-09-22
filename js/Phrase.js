6763bf1505c811953f9f870a1d775f2eb065ef21
Elijas <49196132+elijaspakalnis@users.noreply.github.com>
2020-09-22 15:23:36 +0100

    add Phrase class and addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  addPhraseToDisplay() {
+    let placeHolderHTML = ``;
+    // for each character in phrase, check if it's empty space or letter append either space or letter li element to placeHolderHTML
+    [...this.phrase].forEach(char => placeHolderHTML += (char === ' '? `<li class="space"> </li>` : `<li class="hide letter ${char}">${char}</li>`));
+    
+    // get the dom reference of phrase div ul & set the li elements
+    const phraseUL = document.querySelector('#phrase ul');
+    phraseUL.innerHTML = placeHolderHTML;
+  }
+}
