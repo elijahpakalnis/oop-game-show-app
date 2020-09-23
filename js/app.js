ef15c99a116ad40a94d6300880cb72476f23bd73
Elijas <49196132+elijaspakalnis@users.noreply.github.com>
2020-09-23 19:56:46 +0100

    add keyboard functionality

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-const game = new Game(['keep it simple', 'lets go', 'good stuff']);
+let game;
 
 // start button reference
 const startButton = document.getElementById('btn__reset');
 // click event event listener on button to start game
 startButton.addEventListener('click', e => {
+  game = new Game();
   game.resetGame();
   game.startGame()
-
 });
 
 // click event listener for all key buttons
@@ -15,4 +15,9 @@ keyboard.addEventListener('click', e => {
   if(e.target.tagName === 'BUTTON'){
     game.handleInteraction(e.target.textContent);
   }
+});
+
+// keyup listener so keyboard can be used
+document.addEventListener('keyup', e => {
+  game.handleInteraction(e.key);
 });
